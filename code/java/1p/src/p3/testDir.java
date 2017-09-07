c4df6761f9ff2b28a2a8ec002a64bb4e8352627a
Christolml <5d@iMac-de-iMac21.local>
2017-09-07 13:10:38 -0500

    CopyBytes
    Bytes

---
+++ code/java/1p/src/p3/testDir.java
@@ -0,0 +1,15 @@
+package p3;
+
+
+public class testDir 
+{
+
+
+    public static void main(String[] args) 
+    {
+        System.out.println(System.getProperty("user.dir"));
+        
+        
+    }
+    
+}
